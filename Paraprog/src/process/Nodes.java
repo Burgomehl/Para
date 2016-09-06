e104e3b01966c7d1ae5aa28650d8b4206a419a8d
Benjamin Byl <BenjaminByl@web.de>
2016-09-06 12:46:40 +0200

    ...

--- Paraprog/src/process/Nodes.java
+++ Paraprog/src/process/Nodes.java
@@ -1,10 +1,11 @@
 package process;
+
 import java.util.concurrent.CountDownLatch;
 
 import node.Node;
 import node.NodeAbstract;
 
-public class Nodes extends NodeAbstract implements Runnable{
+public class Nodes extends NodeAbstract implements Runnable {
 	private int countedEchos = 0;
 
 	public Nodes(String name, boolean initiator, CountDownLatch startLatch) {
@@ -13,32 +14,43 @@ public class Nodes extends NodeAbstract implements Runnable{
 
 	@Override
 	public void hello(Node neighbour) {
-		if(!neighbours.contains(neighbour)){
+		if (!neighbours.contains(neighbour)) {
 			neighbours.add(neighbour);
 		}
 	}
 
 	@Override
-	public void wakeup(Node neighbour) {
-		if(neighbours.size()<=1){
+	public synchronized void wakeup(Node neighbour) {
+		++countedEchos;
+		if (neighbours.size() <= 1) {
 			neighbour.echo(this, null);
-		}else{
-			//wait for echo of all neighbours
-			if(countedEchos >= neighbours.size()){
-				neighbour.echo(this, null);
+		} else {
+			// wait for echo of all neighbours
+			while (true) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if (countedEchos >= neighbours.size()) {
+					neighbour.echo(this, null);
+					continue;
+				}
 			}
 		}
 	}
 
 	@Override
 	public void echo(Node neighbour, Object data) {
-		//wait for all neightbours sending an echo
+		// wait for all neightbours sending an echo
 		++countedEchos;
+		notify();
 	}
 
 	@Override
 	public void setupNeighbours(Node... neighbours) {
 		for (Node node : neighbours) {
+			this.neighbours.add(node);
 			node.hello(this);
 		}
 	}
